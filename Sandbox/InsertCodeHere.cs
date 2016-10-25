908f85f1713597fbcc63a2666c0a015566f3ef35
Christian <chri9j83@edu.easj.dk>
2016-10-25 11:22:54 +0200

    Lavet opgave 1-7

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -15,21 +15,21 @@ namespace Sandbox
             aListOfInt.Add(9);
 
             // Case 1
-            // Console.WriteLine("Case 1: Element with index 1 is {0}", aListOfInt[1]);
+             Console.WriteLine("Case 1: Element with index 1 is {0}", aListOfInt[1]);
 
             // Case 2
-            // Console.WriteLine("Case 2: List contains {0} elements", aListOfInt.Count);
+             Console.WriteLine("Case 2: List contains {0} elements", aListOfInt.Count);
  
             aListOfInt.Add(5);
             aListOfInt.Add(22);
 
             // Case 3
-            // Console.WriteLine("Case 3: Element with index 3 is {0}", aListOfInt[3]);
+             Console.WriteLine("Case 3: Element with index 3 is {0}", aListOfInt[3]);
 
             aListOfInt.RemoveAt(0);
 
             // Case 4
-            // Console.WriteLine("Case 4: Element with index 3 is {0}", aListOfInt[3]);
+             Console.WriteLine("Case 4: Element with index 3 is {0}", aListOfInt[3]);
 
 
 
@@ -43,20 +43,34 @@ namespace Sandbox
 
             // Case 5: Make some code that prints out 
             // all the elements in the list
-
+            foreach (int l in aListOfInt)
+            {
+                Console.WriteLine($"Tallet i tisten er: {l}");
+            }
 
 
             // Case 6: Make some code that finds the 
             // sum of the elements in the list, and prints the result
 
-
+            int code = 0;
+            foreach (int list in aListOfInt)
+            {
+                code = code + list;
+            }
+            Console.WriteLine($"Summen er {code}");
 
             // Case 7: Make some code that finds the 
             // average of the elements in the list, and prints the result
             // Tip: Think about how average is defined; then
             // you might reuse something from Case 6
-
-
+            double codes = 0;
+            double count = 0;
+            foreach (int lis in aListOfInt)
+            {
+                codes = codes + lis;
+                count++;
+            }
+            Console.WriteLine($"Avereage is :{codes/count}");
 
             // [DIFFICULT]
             // Case 8: Make some code that finds the
